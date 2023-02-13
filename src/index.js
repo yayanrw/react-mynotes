d44a3f8b9f714b42f6d337f9a0d66a7d7bac3670
yayanrw <yayanraw@gmail.com>
2023-02-13 22:00:41 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import MyNotesApp from "./components/MyNotesApp";
+import { APP_NAME } from "./utils/MyConstants";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+document.title = APP_NAME;
+const root = createRoot(document.getElementById("root"));
+root.render(<MyNotesApp />);
