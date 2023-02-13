cf4d1b3daee37bed54eccdcae73e41a6bca8fd16
yayanrw <yayanraw@gmail.com>
2023-02-13 22:32:18 +0700

    layouting

--- src/components/MyNotesApp.js
+++ src/components/MyNotesApp.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { APP_NAME } from "../utils/MyConstants";
 import SearchAppBar from "./SearchAppBar";
 
 export default class MyNotesApp extends Component {
   render() {
-    return <SearchAppBar />;
+    return (
+      <div>
+        <SearchAppBar />
+      </div>
+    );
   }
 }
