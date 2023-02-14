70a5e72ef4f101d7b92241772ccb6fbf8b09176e
yayanrw <yayanraw@gmail.com>
2023-02-14 11:06:09 +0700

    formatted date

--- src/components/NoteCardItemComponent.js
+++ src/components/NoteCardItemComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Card, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { regular, solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { showFormattedDate } from "../utils/MyData";
 
 const NoteCardItemComponent = ({
   title,
@@ -13,15 +14,15 @@ const NoteCardItemComponent = ({
   onDelete,
 }) => {
   return (
-    <Col lg="3">
-      <Card>
+    <Col lg="3" className="pb-4">
+      <Card className="card-height">
         <Card.Header as="h5" className="p-3">
           {title}
         </Card.Header>
         <Card.Body>
           <Card.Text>{body}</Card.Text>
-          <footer className="blockquote-footer">
-            <cite title="Source Title">Created At</cite> {createdAt}
+          <footer className="blockquote-footer pt-3 mb-0">
+            {showFormattedDate(createdAt)}
           </footer>
         </Card.Body>
         <Card.Footer>
