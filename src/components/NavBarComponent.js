4d4817b273537727330c0ddbd00fe8befbe5455e
yayanrw <yayanraw@gmail.com>
2023-02-14 11:36:47 +0700

    finishing navbar

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -4,9 +4,11 @@ import { APP_NAME } from "../utils/MyConstants";
 
 const NavBarComponent = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" className="pt-4">
       <Container>
-        <Navbar.Brand href="#home">{APP_NAME}</Navbar.Brand>
+        <Navbar.Brand href="#home" style={{ color: "#0DCAF0" }}>
+          {APP_NAME}
+        </Navbar.Brand>
       </Container>
     </Navbar>
   );
