366c6f8c8b9b574a7daafa792b9ae0f03456b3b6
yayanrw <yayanraw@gmail.com>
2023-02-14 10:40:46 +0700

    fix notesapp

--- src/components/MyNotesApp.js
+++ src/components/MyNotesApp.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
-import { SearchNavBar } from "./SearchNavBar";
+import NavBarComponent from "./NavBarComponent";
+import NotesListComponent from "./NotesListComponent";
+import { getInitialData } from "../utils/MyData";
 
 export default class MyNotesApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: getInitialData(),
+    };
+
+    this.onArchiveHandler = this.onArchiveHandler.bind(this);
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+  }
+
+  onArchiveHandler() {
+    // TODO
+  }
+
+  onDeleteHandler() {
+    // TODO
+  }
+
   render() {
     return (
       <div>
-        <SearchNavBar />
+        <NavBarComponent />
+        <NotesListComponent
+          notes={this.state.notes}
+          onArchive={this.onArchiveHandler}
+          onDelete={this.onDeleteHandler}
+        />
       </div>
     );
   }
