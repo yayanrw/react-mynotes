58858c4aa831a5d924da294e90a01b118baecddd
yayanrw <yayanraw@gmail.com>
2023-02-14 11:36:31 +0700

    add more const

--- src/utils/MyConstants.js
+++ src/utils/MyConstants.js
@@ -1,3 +1,6 @@
 const APP_NAME = "MyNotes App";
+const AWESOME_QUOTE =
+  "Creating a notes isn't just about capturing details, it's a space for reflection, clarity, and personal growth. It helps process thoughts, track progress, and cultivate gratitude, making it a powerful tool for self-discovery.";
+const SEARCH_PLACEHOLDER = "Search your awesome notes...";
 
-export { APP_NAME };
+export { APP_NAME, AWESOME_QUOTE, SEARCH_PLACEHOLDER };
