996ecd03baf50152bd88691e2c5205193b4549d6
yayanrw <yayanraw@gmail.com>
2023-02-16 18:26:53 +0700

    remove state to mynotesapp

--- src/components/MyNotesApp.js
+++ src/components/MyNotesApp.js
@@ -1,18 +1,91 @@
-import React from "react";
+import React, { Component } from "react";
 import NavBarComponent from "./NavBarComponent";
 import JumbotronComponent from "./JumbotronComponent";
 import NoteTabsComponent from "./NoteTabsComponent";
 import FooterComponent from "./FooterComponent";
+import { getInitialData } from "../utils/MyData";
+import { ARCHIVE, DELETE, UNARCHIVE } from "../utils/MyConstants";
+import { confirmationDialog, swalSuccess } from "../utils/MyCustoms";
 
-const MyNotesApp = () => {
-  return (
-    <div>
-      <NavBarComponent />
-      <JumbotronComponent />
-      <NoteTabsComponent />
-      <FooterComponent />
-    </div>
-  );
-};
+export class MyNotesApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: getInitialData(),
+    };
+
+    this.onArchiveHandler = this.onArchiveHandler.bind(this);
+    this.onUnarchiveHandler = this.onUnarchiveHandler.bind(this);
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+  }
+
+  onArchiveHandler(id) {
+    confirmationDialog(ARCHIVE, (confirmed) => {
+      if (confirmed) {
+        this.setState((prevState) => ({
+          notes: prevState.notes.map((note) => {
+            if (note.id === id) {
+              return { ...note, archived: true };
+            }
+            return note;
+          }),
+        }));
+        swalSuccess(ARCHIVE);
+      }
+    });
+  }
+
+  onUnarchiveHandler(id) {
+    confirmationDialog(UNARCHIVE, (confirmed) => {
+      if (confirmed) {
+        this.setState((prevState) => ({
+          notes: prevState.notes.map((note) => {
+            if (note.id === id) {
+              return { ...note, archived: false };
+            }
+            return note;
+          }),
+        }));
+        swalSuccess(UNARCHIVE);
+      }
+    });
+  }
+
+  onDeleteHandler(id) {
+    confirmationDialog(DELETE, (confirmed) => {
+      if (confirmed) {
+        this.setState((prevState) => ({
+          notes: prevState.notes.filter((note) => note.id !== id),
+        }));
+        swalSuccess(DELETE);
+      }
+    });
+  }
+
+  onSearchHandler(search) {
+    this.setState((prevState) => ({
+      notes: prevState.notes.filter((note) => {
+        return note.title.toLowerCase().includes(search.toLowerCase());
+      }),
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBarComponent />
+        <JumbotronComponent />
+        <NoteTabsComponent
+          notes={this.state.notes}
+          onArchive={this.onArchiveHandler}
+          onUnarchive={this.onUnarchiveHandler}
+          onDelete={this.onDeleteHandler}
+        />
+        <FooterComponent />
+      </div>
+    );
+  }
+}
 
 export default MyNotesApp;
--- src/components/NoteTabsComponent.js
+++ src/components/NoteTabsComponent.js
@@ -1,137 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import { Container, Tab, Tabs } from "react-bootstrap";
-import {
-  ACTIVE_NOTES,
-  ADD_NOTE,
-  ARCHIVE,
-  ARCHIVED_NOTES,
-  DELETE,
-  UNARCHIVE,
-} from "../utils/MyConstants";
+import { ACTIVE_NOTES, ADD_NOTE, ARCHIVED_NOTES } from "../utils/MyConstants";
 import NotesListComponent from "./NotesListComponent";
-import { getInitialData } from "../utils/MyData";
 import TabTitleComponent from "./TabTitleComponent";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import NoteFormComponent from "./NoteFormComponent";
-import { confirmationDialog, swalSuccess } from "../utils/MyCustoms";
 
-export class NoteTabsComponent extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      notes: getInitialData(),
-    };
-
-    this.onArchiveHandler = this.onArchiveHandler.bind(this);
-    this.onUnarchiveHandler = this.onUnarchiveHandler.bind(this);
-    this.onDeleteHandler = this.onDeleteHandler.bind(this);
-  }
-
-  onArchiveHandler(id) {
-    confirmationDialog(ARCHIVE, (confirmed) => {
-      if (confirmed) {
-        this.setState((prevState) => ({
-          notes: prevState.notes.map((note) => {
-            if (note.id === id) {
-              return { ...note, archived: true };
-            }
-            return note;
-          }),
-        }));
-        swalSuccess(ARCHIVE);
-      }
-    });
-  }
-
-  onUnarchiveHandler(id) {
-    confirmationDialog(UNARCHIVE, (confirmed) => {
-      if (confirmed) {
-        this.setState((prevState) => ({
-          notes: prevState.notes.map((note) => {
-            if (note.id === id) {
-              return { ...note, archived: false };
-            }
-            return note;
-          }),
-        }));
-        swalSuccess(UNARCHIVE);
-      }
-    });
-  }
-
-  onDeleteHandler(id) {
-    confirmationDialog(DELETE, (confirmed) => {
-      if (confirmed) {
-        this.setState((prevState) => ({
-          notes: prevState.notes.filter((note) => note.id !== id),
-        }));
-        swalSuccess(DELETE);
-      }
-    });
-  }
-
-  render() {
-    return (
-      <Container style={{ minHeight: "800px" }}>
-        <Tabs
-          defaultActiveKey="active-notes"
-          id="fill-tab-notes"
-          className="mb-5 mt-5"
-          fill
+const NoteTabsComponent = ({ notes, onArchive, onUnarchive, onDelete }) => {
+  return (
+    <Container style={{ minHeight: "800px" }}>
+      <Tabs
+        defaultActiveKey="active-notes"
+        id="fill-tab-notes"
+        className="mb-5 mt-5"
+        fill
+      >
+        <Tab
+          eventKey="add-note"
+          title={
+            <TabTitleComponent
+              title={ADD_NOTE}
+              count={<FontAwesomeIcon icon={solid("plus")} />}
+            />
+          }
         >
-          <Tab
-            eventKey="add-note"
-            title={
-              <TabTitleComponent
-                title={ADD_NOTE}
-                count={<FontAwesomeIcon icon={solid("plus")} />}
-              />
-            }
-          >
-            <NoteFormComponent />
-          </Tab>
-          <Tab
-            eventKey="active-notes"
-            title={
-              <TabTitleComponent
-                title={ACTIVE_NOTES}
-                count={this.state.notes.filter((item) => !item.archived).length}
-                badgeType="primary"
-              />
-            }
-          >
-            <NotesListComponent
-              notes={this.state.notes}
-              showArchive={false}
-              onArchive={this.onArchiveHandler}
-              onUnarchive={this.onUnarchiveHandler}
-              onDelete={this.onDeleteHandler}
+          <NoteFormComponent />
+        </Tab>
+        <Tab
+          eventKey="active-notes"
+          title={
+            <TabTitleComponent
+              title={ACTIVE_NOTES}
+              count={notes.filter((item) => !item.archived).length}
+              badgeType="primary"
             />
-          </Tab>
-          <Tab
-            eventKey="archived-notes"
-            title={
-              <TabTitleComponent
-                title={ARCHIVED_NOTES}
-                count={this.state.notes.filter((item) => item.archived).length}
-                badgeType="secondary"
-              />
-            }
-          >
-            <NotesListComponent
-              notes={this.state.notes}
-              showArchive={true}
-              onArchive={this.onArchiveHandler}
-              onUnarchive={this.onUnarchiveHandler}
-              onDelete={this.onDeleteHandler}
+          }
+        >
+          <NotesListComponent
+            notes={notes}
+            showArchive={false}
+            onArchive={onArchive}
+            onUnarchive={onUnarchive}
+            onDelete={onDelete}
+          />
+        </Tab>
+        <Tab
+          eventKey="archived-notes"
+          title={
+            <TabTitleComponent
+              title={ARCHIVED_NOTES}
+              count={notes.filter((item) => item.archived).length}
+              badgeType="secondary"
             />
-          </Tab>
-        </Tabs>
-      </Container>
-    );
-  }
-}
+          }
+        >
+          <NotesListComponent
+            notes={notes}
+            showArchive={true}
+            onArchive={onArchive}
+            onUnarchive={onUnarchive}
+            onDelete={onDelete}
+          />
+        </Tab>
+      </Tabs>
+    </Container>
+  );
+};
 
 export default NoteTabsComponent;
