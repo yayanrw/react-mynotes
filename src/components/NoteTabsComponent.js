7d6e4eaaeeb82080e58488453a09d754d895c125
yayanrw <yayanraw@gmail.com>
2023-05-01 09:04:20 +0700

    using tab as Link

--- src/components/NoteTabsComponent.js
+++ src/components/NoteTabsComponent.js
@@ -1,16 +1,56 @@
 import React from "react";
-import { Container, Tab, Tabs } from "react-bootstrap";
+import { Container, Nav, Tab, Tabs } from "react-bootstrap";
 import { ACTIVE_NOTES, ADD_NOTE, ARCHIVED_NOTES } from "../utils/MyConstants";
 import NotesListComponent from "./NotesListComponent";
 import TabTitleComponent from "./TabTitleComponent";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import NoteFormComponent from "./NoteFormComponent";
+import { Link } from "react-router-dom";
 
-const NoteTabsComponent = ({ notes, onAddNotes, onArchive, onUnarchive, onDelete }) => {
+const NoteTabsComponent = ({
+  notes,
+  onAddNotes,
+  onArchive,
+  onUnarchive,
+  onDelete,
+}) => {
   return (
     <Container style={{ minHeight: "800px" }}>
-      <Tabs
+      <Nav
+        justify
+        variant="tabs"
+        defaultActiveKey="/active-notes"
+        className="mb-5 mt-5"
+      >
+        <Nav.Item>
+          <Nav.Link as={Link} to="/add-notes" eventKey="/add-notes">
+            <TabTitleComponent
+              title={ADD_NOTE}
+              count={<FontAwesomeIcon icon={solid("plus")} />}
+            />
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/active-notes" eventKey="/active-notes">
+            <TabTitleComponent
+              title={ACTIVE_NOTES}
+              count={notes.filter((item) => !item.archived).length}
+              badgeType="primary"
+            />
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/archived-notes" eventKey="/archived-notes">
+            <TabTitleComponent
+              title={ARCHIVED_NOTES}
+              count={notes.filter((item) => item.archived).length}
+              badgeType="secondary"
+            />
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      {/* <Tabs
         defaultActiveKey="active-notes"
         id="fill-tab-notes"
         className="mb-5 mt-5"
@@ -63,7 +103,7 @@ const NoteTabsComponent = ({ notes, onAddNotes, onArchive, onUnarchive, onDelete
             onDelete={onDelete}
           />
         </Tab>
-      </Tabs>
+      </Tabs> */}
     </Container>
   );
 };
