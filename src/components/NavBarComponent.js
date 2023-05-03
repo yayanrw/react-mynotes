6e7642831d5a681b55f90a773c9c98a6d74ce36a
yayanrw <yayanraw@gmail.com>
2023-05-03 15:49:25 +0700

    using locale

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
-import { APP_NAME } from "../utils/MyConstants";
+import useLocalization from "../hooks/useLocalization";
 
 const NavBarComponent = () => {
+  const localization = useLocalization("app");
+
   return (
     <Navbar bg="dark" variant="dark" className="pt-4">
       <Container>
         <Navbar.Brand href="#home" style={{ color: "#0DCAF0" }}>
-          {APP_NAME}
+          {localization.appName}
         </Navbar.Brand>
       </Container>
     </Navbar>
