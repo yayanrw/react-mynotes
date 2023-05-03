fdcb54768bb3dd521e45ff0de07c018b3d1bf027
yayanrw <yayanraw@gmail.com>
2023-05-03 16:10:38 +0700

    using locale

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Container, Row, Col, Alert } from "react-bootstrap";
+import useLocalization from "../hooks/useLocalization";
 
 const NotFoundPage = () => {
+  const localization = useLocalization("pageNotFound");
+
   return (
     <Container className="mt-5">
       <Row>
         <Col>
           <Alert variant="danger">
-            <Alert.Heading>404 - Page Not Found</Alert.Heading>
-            <p>The page you are looking for does not exist.</p>
+            <Alert.Heading>{localization.message}</Alert.Heading>
+            <p>{localization.detailMessage}</p>
           </Alert>
         </Col>
       </Row>
