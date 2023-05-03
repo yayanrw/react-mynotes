c13d575a370a11214034c871794a60403db3235d
yayanrw <yayanraw@gmail.com>
2023-05-03 15:44:45 +0700

    move mynotesapp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import MyNotesApp from "./components/MyNotesApp";
 import { APP_NAME } from "./utils/MyConstants";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/MyStyles.css";
+import MyNotesApp from "./MyNotesApp";
 
 document.title = APP_NAME;
 const root = createRoot(document.getElementById("root"));
