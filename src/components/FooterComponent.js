f95321cb62928b6c986d88f117de4a5f04bb26d3
yayanrw <yayanraw@gmail.com>
2023-05-05 09:54:07 +0700

    remove

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { AUTHOR, CLASS_NAME } from "../utils/MyConstants";
+import { AUTHOR } from "../utils/MyConstants";
 import useLocalization from "../hooks/useLocalization";
 
 const FooterComponent = () => {
