bd92325a3a89b3fe6775dd293c2797a3a634d07a
yayanrw <yayanraw@gmail.com>
2023-05-05 09:53:46 +0700

    using locale

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { AUTHOR, CLASS_NAME } from "../utils/MyConstants";
+import useLocalization from "../hooks/useLocalization";
 
 const FooterComponent = () => {
+  const localization = useLocalization("app");
   return (
     <Container fluid className="bg-dark px-5 mt-5">
       <Row>
         <Col>
           <Container>
             <p className="mb-0 text-white pt-5 pb-5">
-              {CLASS_NAME}
+              {localization.courseName}
               <span className="float-end">{AUTHOR} @Dicoding 2023</span>
             </p>
           </Container>
