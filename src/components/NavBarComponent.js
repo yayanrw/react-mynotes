af1dbf2b2c6dc896642e80084742d0b7e06d8efd
yayanrw <yayanraw@gmail.com>
2023-05-05 14:45:54 +0700

    add toggle change lang

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -1,16 +1,23 @@
-import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import useLocalization from "../hooks/useLocalization";
+import LocalizationContext from "../contexts/LocalizationContext";
 
 const NavBarComponent = () => {
-  const localization = useLocalization("app");
+  const localizationApp = useLocalization("app");
+  const { localization, toggleLocalization } = useContext(LocalizationContext);
 
   return (
     <Navbar bg="dark" variant="dark" className="pt-4">
       <Container>
         <Navbar.Brand href="#home" style={{ color: "#0DCAF0" }}>
-          {localization.appName}
+          {localizationApp.appName}
         </Navbar.Brand>
+        <Nav>
+          <Nav.Link onClick={toggleLocalization}>
+            {localization.toUpperCase()}
+          </Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
