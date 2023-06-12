e03ea9216e382b52c8f50b35e323c05efa5e1ba0
yayanrw <yayanraw@gmail.com>
2023-06-12 21:29:36 +0700

    add useTheme.js

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { LIGHT, THEME } from "../utils/MyConstants";
+
+const useTheme = () => {
+  const [theme, setTheme] = useState(LIGHT);
+
+  const onThemeChange = (value) => {
+    setTheme(value);
+
+    const root = window.document.documentElement;
+    root.setAttribute("data-theme", value);
+    localStorage.setItem(THEME, value);
+  };
+
+  return [theme, onThemeChange];
+};
+
+export default useTheme;
