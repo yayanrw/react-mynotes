73440a9d8a95ca0de8e2b3e3cc7f33f35026e651
yayanrw <yayanraw@gmail.com>
2023-06-13 10:47:29 +0700

    fixing toggle theme

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -2,40 +2,38 @@ import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import useLocalization from "../hooks/useLocalization";
 import LocalizationContext from "../contexts/LocalizationContext";
-import { EN, ID, LIGHT } from "../utils/MyConstants";
-import ThemeContext from "../contexts/ThemeContext";
+import { EN_KEY, ID_KEY, LIGHT_KEY } from "../utils/MyConstants";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import useTheme from "../hooks/useTheme";
 
 const NavBarComponent = () => {
   const localizationApp = useLocalization("app");
   const { localization, toggleLocalization } = useContext(LocalizationContext);
-  const { theme, toggleTheme } = useContext(ThemeContext);
+  const [theme, toggleTheme] = useTheme();
 
   return (
-    <Navbar bg="light" variant="light" className="pt-4">
+    <Navbar id="my-nav-bar" bg="light" variant="light" className="pt-4">
       <Container>
         <Navbar.Brand href="#home" style={{ color: "#0DCAF0" }}>
           {localizationApp.appName}
         </Navbar.Brand>
         <Nav>
-          <Nav.Link>
-            {theme === LIGHT ? (
+          <Nav.Link className="text-dark" onClick={toggleTheme}>
+            {theme === LIGHT_KEY ? (
               <FontAwesomeIcon
                 title={localizationApp.applyDarkMode}
-                onClick={toggleTheme}
                 icon={solid("moon")}
               />
             ) : (
               <FontAwesomeIcon
                 title={localizationApp.applyLightMode}
-                onClick={toggleTheme}
                 icon={solid("sun")}
               />
             )}
           </Nav.Link>
-          <Nav.Link onClick={toggleLocalization}>
-            {(localization === ID ? EN : ID).toUpperCase()}
+          <Nav.Link onClick={toggleLocalization} className="text-dark">
+            {(localization === ID_KEY ? EN_KEY : ID_KEY).toUpperCase()}
           </Nav.Link>
         </Nav>
       </Container>
