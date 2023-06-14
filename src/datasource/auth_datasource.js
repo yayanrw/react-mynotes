86a70a6a6a3e0b4c1bc92bdc2c3ca18bd2aa3db5
yayanrw <yayanraw@gmail.com>
2023-06-14 08:40:46 +0700

    feat: create auth_datasource.js

---
+++ src/datasource/auth_datasource.js
@@ -0,0 +1,37 @@
+import { BASE_URL } from "./MyConstants";
+import { defaultHeaders, fetchApi, headersWithToken } from "../utils/network_helper";
+
+const fetchLogin = async ({ email, password }) => {
+  const url = `${BASE_URL}/login`;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  };
+
+  return fetchApi(url, options);
+};
+
+const fetchRegister = async ({ name, email, password }) => {
+  const url = `${BASE_URL}/register`;
+  const options = {
+    method: "POST",
+    headers: defaultHeaders,
+    body: JSON.stringify({ name, email, password }),
+  };
+
+  return fetchApi(url, options);
+};
+
+const fetchLoggedUser = async () => {
+    const url = `${BASE_URL}/user`;
+    const options = {
+      method: "GET",
+      headers: headersWithToken,
+    };
+    return fetchApi(url, options);
+}
+
+export { fetchLogin, fetchRegister, fetchLoggedUser };
