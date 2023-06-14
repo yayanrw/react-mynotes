8eb9adeeebe773e90fd93f1031bc2c7e41cf7caa
yayanrw <yayanraw@gmail.com>
2023-06-14 08:36:50 +0700

    feat: create local_storage_datasource.js

---
+++ src/datasource/local_storage_datasource.js
@@ -0,0 +1,39 @@
+import { LOCALIZATION_KEY, THEME_KEY, TOKEN_KEY } from "../utils/MyConstants";
+
+const getToken = () => {
+  return localStorage.getItem(TOKEN_KEY);
+};
+
+const setToken = (token) => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
+
+const removeToken = () => {
+  localStorage.removeItem(TOKEN_KEY);
+};
+
+const getTheme = () => {
+  return localStorage.getItem(THEME_KEY);
+};
+
+const setTheme = (theme) => {
+  localStorage.setItem(THEME_KEY, theme);
+};
+
+const getLocalization = () => {
+  return localStorage.getItem(LOCALIZATION_KEY);
+};
+
+const setLocalization = (localization) => {
+  localStorage.setItem(LOCALIZATION_KEY, localization);
+};
+
+export {
+  getToken,
+  setToken,
+  removeToken,
+  getTheme,
+  setTheme,
+  getLocalization,
+  setLocalization,
+};
