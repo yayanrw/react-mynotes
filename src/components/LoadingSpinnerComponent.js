bf11036a2e51ec335271bbd9e6c0e37aae286d21
yayanrw <yayanraw@gmail.com>
2023-06-15 10:37:01 +0700

    update LoadingSpinnerComponent.js

--- src/components/LoadingSpinnerComponent.js
+++ src/components/LoadingSpinnerComponent.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Container, Spinner } from "react-bootstrap";
+import { Col, Spinner } from "react-bootstrap";
 
 const LoadingSpinnerComponent = () => {
   return (
-    <Container
+    <Col
       style={{
         minHeight: "800px",
       }}
-      className="my-background-lighter pt-5 d-flex align-items-center"
+      className="text-center"
     >
       <Spinner animation="border" role="status" variant="info" />
-    </Container>
+    </Col>
   );
 };
 
