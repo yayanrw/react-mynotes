d460c3173d9325a104e6d590d892682f9f726e9c
yayanrw <yayanraw@gmail.com>
2023-06-15 16:01:31 +0700

    update DetailPage.js

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,15 +1,19 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Badge, Card, Col, Form, Row } from "react-bootstrap";
 import { showFormattedDate } from "../utils/date_helper";
-import { getNote } from "../utils/MyData";
 import { useParams } from "react-router-dom";
 import useLocalization from "../hooks/useLocalization";
 import LocalizationContext from "../contexts/LocalizationContext";
 import { EN_LANG, ID_KEY, ID_LANG } from "../utils/constants";
+import { fetchNote } from "../datasources/note_datasource";
+import { ApplicationException, ServerException } from "../utils/exceptions";
+import { swalError, swalWarning } from "../utils/swal_helper";
+import LoadingSpinnerComponent from "../components/LoadingSpinnerComponent";
 
 const DetailPage = () => {
   const { id } = useParams();
   const [note, setNote] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const localizationCard = useLocalization("card");
   const localizationInput = useLocalization("input");
   const localizationSwal = useLocalization("swal");
@@ -17,8 +21,26 @@ const DetailPage = () => {
   const { localization } = useContext(LocalizationContext);
   const lang = localization === ID_KEY ? ID_LANG : EN_LANG;
 
+  const callDetailNote = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await fetchNote(id);
+      setNote(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      if (error instanceof ApplicationException) {
+        swalWarning(localizationSwal.warning, error.message);
+      } else if (error instanceof ServerException) {
+        swalError(localizationSwal.serverError, error.message);
+      } else {
+        swalError(localizationSwal.anErrorOccured, error.message);
+      }
+    }
+  };
+
   useEffect(() => {
-    setNote(getNote(Number(id)));
+    callDetailNote();
 
     return () => {
       setNote(null);
@@ -26,58 +48,64 @@ const DetailPage = () => {
   }, [id]);
 
   return (
-    <Row>
-      {note === null || note === undefined ? (
-        <Col className="text-center mt-5 pt-5" as="h5">
-          {localizationSwal.noNotes}
-        </Col>
+    <>
+      {isLoading ? (
+        <LoadingSpinnerComponent />
       ) : (
-        <Col>
-          <Card className="card-height">
-            <Card.Header as="h5" className="p-3">
-              {localizationCard.detailNote}{" "}
-              <Badge bg={note.archived ? "success" : "warning"}>
-                {note.archived
-                  ? localizationCard.archive
-                  : localizationCard.unArchive}
-              </Badge>
-            </Card.Header>
-            <Card.Body>
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicTitle">
-                  <Form.Label>{localizationInput.title}</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder={localizationInput.titlePlaceholder}
-                    value={note.title}
-                    disabled
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicBody">
-                  <Form.Label>{localizationInput.detail}</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    rows={5}
-                    placeholder={localizationInput.detailPlaceholder}
-                    value={note.body}
-                    disabled
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicTitle">
-                  <Form.Label>{localizationInput.createdAt}</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder={localizationInput.createdAt}
-                    value={showFormattedDate(note.createdAt, lang)}
-                    disabled
-                  />
-                </Form.Group>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
+        <Row>
+          {note === null || note === undefined ? (
+            <Col className="text-center mt-5 pt-5" as="h5">
+              {localizationSwal.noNotes}
+            </Col>
+          ) : (
+            <Col>
+              <Card className="card-height">
+                <Card.Header as="h5" className="p-3">
+                  {localizationCard.detailNote}{" "}
+                  <Badge bg={note.archived ? "success" : "warning"}>
+                    {note.archived
+                      ? localizationCard.archive
+                      : localizationCard.unArchive}
+                  </Badge>
+                </Card.Header>
+                <Card.Body>
+                  <Form>
+                    <Form.Group className="mb-3" controlId="formBasicTitle">
+                      <Form.Label>{localizationInput.title}</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder={localizationInput.titlePlaceholder}
+                        value={note.title}
+                        disabled
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicBody">
+                      <Form.Label>{localizationInput.detail}</Form.Label>
+                      <Form.Control
+                        as="textarea"
+                        rows={5}
+                        placeholder={localizationInput.detailPlaceholder}
+                        value={note.body}
+                        disabled
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicTitle">
+                      <Form.Label>{localizationInput.createdAt}</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder={localizationInput.createdAt}
+                        value={showFormattedDate(note.createdAt, lang)}
+                        disabled
+                      />
+                    </Form.Group>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+          )}
+        </Row>
       )}
-    </Row>
+    </>
   );
 };
 
