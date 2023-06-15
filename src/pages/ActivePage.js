93b68089814ff5eae28977f048fb4c24d6ee05a9
yayanrw <yayanraw@gmail.com>
2023-06-15 13:11:55 +0700

    update ActivePage.js

--- src/pages/ActivePage.js
+++ src/pages/ActivePage.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import NotesListComponent from "../components/NotesListComponent";
-import PropTypes from "prop-types";
 import { fetchActiveNotes } from "../datasources/note_datasource";
 import { ApplicationException, ServerException } from "../utils/exceptions";
 import { swalError, swalWarning } from "../utils/swal_helper";
 import useLocalization from "../hooks/useLocalization";
 import LoadingSpinnerComponent from "../components/LoadingSpinnerComponent";
 
-const ActivePage = ({ onArchive, onUnarchive, onDelete }) => {
+const ActivePage = () => {
   const [notes, setNotes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const localizationSwal = useLocalization("swal");
@@ -40,23 +39,9 @@ const ActivePage = ({ onArchive, onUnarchive, onDelete }) => {
       {isLoading ? (
         <LoadingSpinnerComponent />
       ) : (
-        <NotesListComponent
-          notes={notes}
-          showArchive={false}
-          onArchive={onArchive}
-          onUnarchive={onUnarchive}
-          onDelete={onDelete}
-        />
+        <NotesListComponent notes={notes} />
       )}
     </>
   );
 };
-
-ActivePage.propTypes = {
-  notes: PropTypes.arrayOf(PropTypes.object).isRequired,
-  onArchive: PropTypes.func.isRequired,
-  onUnarchive: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
-};
-
 export default ActivePage;
