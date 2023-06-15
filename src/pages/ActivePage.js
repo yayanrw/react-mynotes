84cb1012a0b43bd24f2991a1e150d36d39cb1f23
yayanrw <yayanraw@gmail.com>
2023-06-15 10:37:10 +0700

    update ActivePage.js

--- src/pages/ActivePage.js
+++ src/pages/ActivePage.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NotesListComponent from "../components/NotesListComponent";
 import PropTypes from "prop-types";
+import { fetchActiveNotes } from "../datasources/note_datasource";
+import { ApplicationException, ServerException } from "../utils/exceptions";
+import { swalError, swalWarning } from "../utils/swal_helper";
+import useLocalization from "../hooks/useLocalization";
+import LoadingSpinnerComponent from "../components/LoadingSpinnerComponent";
 
-const ActivePage = ({ notes, onArchive, onUnarchive, onDelete }) => {
+const ActivePage = ({ onArchive, onUnarchive, onDelete }) => {
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const localizationSwal = useLocalization("swal");
+
+  const callNotesFromApi = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await fetchActiveNotes();
+
+      setIsLoading(false);
+      setNotes(data);
+    } catch (error) {
+      setIsLoading(false);
+      if (error instanceof ApplicationException) {
+        swalWarning(localizationSwal.warning, error.message);
+      } else if (error instanceof ServerException) {
+        swalError(localizationSwal.serverError, error.message);
+      } else {
+        swalError(localizationSwal.anErrorOccured, error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    callNotesFromApi();
+  }, []);
+
   return (
-    <NotesListComponent
-      notes={notes}
-      showArchive={false}
-      onArchive={onArchive}
-      onUnarchive={onUnarchive}
-      onDelete={onDelete}
-    />
+    <>
+      {isLoading ? (
+        <LoadingSpinnerComponent />
+      ) : (
+        <NotesListComponent
+          notes={notes}
+          showArchive={false}
+          onArchive={onArchive}
+          onUnarchive={onUnarchive}
+          onDelete={onDelete}
+        />
+      )}
+    </>
   );
 };
 
