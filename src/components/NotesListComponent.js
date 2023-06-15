0801aa998de7374340ff3d1b03cd50776f8a2fc0
yayanrw <yayanraw@gmail.com>
2023-06-15 10:36:56 +0700

    update NotesListComponent.js

--- src/components/NotesListComponent.js
+++ src/components/NotesListComponent.js
@@ -4,27 +4,20 @@ import NoteCardItemComponent from "./NoteCardItemComponent";
 import PropTypes from "prop-types";
 import useLocalization from "../hooks/useLocalization";
 
-const NotesListComponent = ({
-  notes,
-  showArchive = false,
-  onArchive,
-  onUnarchive,
-  onDelete,
-}) => {
-  const filteredNotes = notes.filter((note) => note.archived === showArchive);
+const NotesListComponent = ({ notes }) => {
   const localization = useLocalization("swal");
 
   return (
     <Row>
-      {filteredNotes.length > 0 ? (
-        filteredNotes.map((note) => {
+      {notes.length > 0 ? (
+        notes.map((note) => {
           return (
             <NoteCardItemComponent
               key={note.id}
               id={note.id}
-              onArchive={onArchive}
-              onUnarchive={onUnarchive}
-              onDelete={onDelete}
+              onArchive={"onArchive"}
+              onUnarchive={"onUnarchive"}
+              onDelete={"onDelete"}
               {...note}
             />
           );
@@ -40,10 +33,6 @@ const NotesListComponent = ({
 
 NotesListComponent.propTypes = {
   notes: PropTypes.arrayOf(PropTypes.object).isRequired,
-  showArchive: PropTypes.bool,
-  onArchive: PropTypes.func.isRequired,
-  onUnarchive: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
 };
 
 export default NotesListComponent;
