91d4b80e1269c90a3a579e39c1e287fa3c247fc9
yayanrw <yayanraw@gmail.com>
2023-06-15 10:22:06 +0700

    update BaseNoteComponent.js

--- src/components/BaseNoteComponent.js
+++ src/components/BaseNoteComponent.js
@@ -1,94 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Route, Routes, useSearchParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import JumbotronComponent from "./JumbotronComponent";
 import NavigationComponent from "./NavigationComponent";
 import { Container } from "react-bootstrap";
-import AddPage from "../pages/AddPage";
-import ActivePage from "../pages/ActivePage";
-import ArchivePage from "../pages/ArchivePage";
-import DetailPage from "../pages/DetailPage";
-import NotFoundPage from "../pages/NotFoundPage";
-import { addNote, archivedNote, deleteNote, getNotes } from "../utils/MyData";
-import { confirmationDialog, swalSuccess } from "../utils/swal_helper";
-import useLocalization from "../hooks/useLocalization";
 
-const BaseNoteComponent = () => {
+const BaseNoteComponent = ({ child }) => {
   const [searchParams, setSearchParams] = useSearchParams();
-
-  const [notes, setNotes] = useState([]);
   const [keyword, setKeyword] = useState(() => {
     return searchParams.get("keyword") || "";
   });
-
-  const localization = useLocalization("swal");
-
-  useEffect(() => {
-    setNotes(getNotes());
-  }, []);
-
-  const onAddNotesHandler = (contact) => {
-    addNote(contact);
-    swalSuccess(localization.success, localization.insertSuggest);
-
-    setNotes(getNotes());
-  };
 
   const onKeywordChangeHandler = (keyword) => {
     setKeyword(keyword);
     setSearchParams({ keyword });
   };
-
-  const onDeleteHandler = (id) => {
-    confirmationDialog(
-      localization.deleteDataWarn,
-      localization.deleteIt,
-      localization.areYouSure,
-      (confirmed) => {
-        if (confirmed) {
-          deleteNote(id);
-          swalSuccess(localization.success, localization.deleteSuggest);
-
-          setNotes(getNotes());
-        }
-      }
-    );
-  };
-
-  const onArchiveHandler = (id) => {
-    confirmationDialog(
-      localization.archiveDataWarn,
-      localization.archiveIt,
-      localization.areYouSure,
-      (confirmed) => {
-        if (confirmed) {
-          archivedNote(id, true);
-          swalSuccess(localization.success, localization.archiveSuggest);
-
-          setNotes(getNotes());
-        }
-      }
-    );
-  };
-
-  const onUnarchiveHandler = (id) => {
-    confirmationDialog(
-      localization.unArchiveDataWarn,
-      localization.unArchiveIt,
-      localization.areYouSure,
-      (confirmed) => {
-        if (confirmed) {
-          archivedNote(id, false);
-          swalSuccess(localization.success, localization.unArchiveSuggest);
-
-          setNotes(getNotes());
-        }
-      }
-    );
-  };
-
-  const filteredNotes = notes.filter((note) => {
-    return note.title.toLowerCase().includes(keyword.toLowerCase());
-  });
 
   return (
     <>
@@ -96,39 +21,8 @@ const BaseNoteComponent = () => {
         keyword={keyword}
         onKeywordChangeHandler={onKeywordChangeHandler}
       />
-      <NavigationComponent notes={filteredNotes} />
-      <Container style={{ minHeight: "800px" }}>
-        <Routes>
-          <Route
-            path="/add-notes"
-            element={<AddPage onAddNotes={onAddNotesHandler} />}
-          />
-          <Route
-            path="/"
-            element={
-              <ActivePage
-                notes={filteredNotes}
-                onArchive={onArchiveHandler}
-                onUnarchive={onUnarchiveHandler}
-                onDelete={onDeleteHandler}
-              />
-            }
-          />
-          <Route
-            path="/archived-notes"
-            element={
-              <ArchivePage
-                notes={filteredNotes}
-                onArchive={onArchiveHandler}
-                onUnarchive={onUnarchiveHandler}
-                onDelete={onDeleteHandler}
-              />
-            }
-          />
-          <Route path="/detail-note/:id" element={<DetailPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Container>
+      <NavigationComponent />
+      <Container style={{ minHeight: "800px" }}>{child}</Container>
     </>
   );
 };
