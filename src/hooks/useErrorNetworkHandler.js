d3b15fabe3f17d70887ba2f2b120c7a70a943bbe
yayanrw <yayanraw@gmail.com>
2023-06-17 12:54:48 +0700

    feat: create useErrorNetworkHandler.js

---
+++ src/hooks/useErrorNetworkHandler.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import useLocalization from "./useLocalization";
+import { swalError, swalWarning } from "../utils/swal_helper";
+import { ApplicationException, ServerException } from "../utils/exceptions";
+
+const useErrorNetworkHandler = () => {
+  const [error, setError] = useState({});
+  const localizationSwal = useLocalization("swal");
+
+  const handleApiError = (error) => {
+    if (error instanceof ApplicationException) {
+      swalWarning(localizationSwal.warning, error.message);
+    } else if (error instanceof ServerException) {
+      swalError(localizationSwal.serverError, error.message);
+    } else {
+      swalError(localizationSwal.anErrorOccured, error.message);
+    }
+    setError(error);
+  };
+
+  return {
+    error,
+    handleApiError,
+  };
+};
+
+export default useErrorNetworkHandler;
