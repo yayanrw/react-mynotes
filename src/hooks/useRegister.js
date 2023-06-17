a37235719353f888a571d5178cde7b2ad78f0cd5
yayanrw <yayanraw@gmail.com>
2023-06-17 14:13:43 +0700

    feat: create useRegister.js

---
+++ src/hooks/useRegister.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import useErrorNetworkHandler from "./useErrorNetworkHandler";
+import { fetchRegister } from "../datasources/auth_datasource";
+import { swalSuccess } from "../utils/swal_helper";
+import useLocalization from "./useLocalization";
+
+const useRegister = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const localizationSwal = useLocalization("swal");
+  const { handleApiError } = useErrorNetworkHandler();
+
+  const handleSubmit = async () => {
+    try {
+      setIsLoading(true);
+      await fetchRegister({ name: name, email: email, password: password });
+      resetState();
+      swalSuccess(localizationSwal.success, localizationSwal.registerSuggest);
+    } catch (error) {
+      setIsLoading(false);
+      handleApiError(error);
+    }
+  };
+
+  const resetState = () => {
+    setName("");
+    setEmail("");
+    setPassword("");
+    setIsLoading(false);
+  };
+
+  return {
+    setName,
+    setEmail,
+    setPassword,
+    isLoading,
+    handleSubmit,
+  };
+};
+
+export default useRegister;
