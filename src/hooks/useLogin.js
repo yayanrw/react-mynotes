1e0923594b0efd1f8030f7b85d313de77c761c9d
yayanrw <yayanraw@gmail.com>
2023-06-17 12:59:14 +0700

    feat: create useLogin.js

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,33 @@
+import { useContext, useState } from "react";
+import { fetchLogin } from "../datasources/auth_datasource";
+import AuthContext from "../contexts/AuthContext";
+import useErrorNetworkHandler from "./useErrorNetworkHandler";
+import { setToken } from "../datasources/local_storage_datasource";
+
+const useLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { setAuth } = useContext(AuthContext);
+  const { handleApiError } = useErrorNetworkHandler();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const { data } = await fetchLogin({ email: email, password: password });
+      setToken(data.accessToken);
+      setAuth(1);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      handleApiError(error);
+    }
+  };
+
+  return [email, setEmail, password, setPassword, isLoading, handleSubmit];
+};
+
+export default useLogin;
