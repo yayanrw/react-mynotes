3e3256812a27dab3ca3309e4afed7c816e99c81a
yayanrw <yayanraw@gmail.com>
2023-06-23 14:38:59 +0700

    update ArchivePage.js

--- src/pages/ArchivePage.js
+++ src/pages/ArchivePage.js
@@ -1,37 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import NotesListComponent from "../components/NotesListComponent";
-import { fetchArchivedNotes } from "../datasources/note_datasource";
-import useLocalization from "../hooks/useLocalization";
-import { ApplicationException, ServerException } from "../utils/exceptions";
-import { swalError, swalWarning } from "../utils/swal_helper";
 import LoadingSpinnerComponent from "../components/LoadingSpinnerComponent";
+import useNotes from "../hooks/useNotes";
 
 const ArchivePage = () => {
-  const [notes, setNotes] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const localizationSwal = useLocalization("swal");
-
-  const callNotesFromApi = async () => {
-    try {
-      setIsLoading(true);
-      const { data } = await fetchArchivedNotes();
-
-      setIsLoading(false);
-      setNotes(data);
-    } catch (error) {
-      setIsLoading(false);
-      if (error instanceof ApplicationException) {
-        swalWarning(localizationSwal.warning, error.message);
-      } else if (error instanceof ServerException) {
-        swalError(localizationSwal.serverError, error.message);
-      } else {
-        swalError(localizationSwal.anErrorOccured, error.message);
-      }
-    }
-  };
+  const { handleGetArchivedNotes, isLoading, filteredNotes } = useNotes();
 
   useEffect(() => {
-    callNotesFromApi();
+    handleGetArchivedNotes();
   }, []);
 
   return (
@@ -39,7 +15,7 @@ const ArchivePage = () => {
       {isLoading ? (
         <LoadingSpinnerComponent />
       ) : (
-        <NotesListComponent notes={notes} />
+        <NotesListComponent notes={filteredNotes} />
       )}
     </>
   );
