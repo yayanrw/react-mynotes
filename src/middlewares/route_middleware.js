a82c2db08907a0e3dd7f13650bbef65c310f5c92
yayanrw <yayanraw@gmail.com>
2023-06-23 11:21:02 +0700

    update route_middleware.js

--- src/middlewares/route_middleware.js
+++ src/middlewares/route_middleware.js
@@ -1,11 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AuthContext from "../contexts/AuthContext";
 import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
+import useAuth from "../hooks/useAuth";
+import LoadingSpinnerComponent from "../components/LoadingSpinnerComponent";
 
 const RouteMiddleware = ({ child, middleware }) => {
   const { auth } = useContext(AuthContext);
   const location = useLocation();
+  const { isLoading, handleLoggedUser } = useAuth();
+
+  useEffect(() => {
+    handleLoggedUser();
+  }, []);
+
+  if (isLoading) {
+    return <LoadingSpinnerComponent />;
+  }
 
   const middlewares = ["auth", "public"];
 
