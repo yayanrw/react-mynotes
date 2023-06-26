a15b5c49f324aa318fe340d3495178fc9ed7059e
yayanrw <yayanraw@gmail.com>
2023-06-26 21:49:19 +0700

    bug

--- src/utils/date_helper.js
+++ src/utils/date_helper.js
@@ -1,16 +1,15 @@
 const showFormattedDate = (date, lang) => {
-  if (!date) {
-    return "";
+  if (date && typeof date === "object") {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Date(date).toLocaleDateString(lang, options);
   }
 
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-
-  return new Date(date).toLocaleDateString(lang, options);
+  return ""; // or any default value you prefer
 };
 
 export { showFormattedDate };
