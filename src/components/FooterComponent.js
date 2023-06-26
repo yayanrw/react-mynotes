745064e76fc0265b7688541f369d83474b0b6cd6
yayanrw <yayanraw@gmail.com>
2023-06-26 17:58:40 +0700

    fixing footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -7,13 +7,9 @@ const FooterComponent = () => {
   const localization = useLocalization("app");
   return (
     <Container fluid className="my-background px-5">
-      <Row>
-        <Col>
-          <Container className="d-flex flex-md-row flex-column justify-content-between align-items-center pt-5 pb-5">
-            <span className="py-2">{localization.courseName}</span>
-            <span className="py-2">{AUTHOR} @Dicoding 2023</span>
-          </Container>
-        </Col>
+      <Row className="d-flex flex-md-row flex-column justify-content-between align-items-center pt-5 pb-5">
+        <Col className="py-2 text-center">{localization.courseName}</Col>
+        <Col className="py-2 text-center">{AUTHOR} @Dicoding 2023</Col>
       </Row>
     </Container>
   );
